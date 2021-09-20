443e91c460f73b9435b588cad9165eb83cbca6fd
GTRONICK <gtronick@gmail.com>
2021-09-20 09:21:39 -0500

    Added initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { SharedModule } from './layout/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
